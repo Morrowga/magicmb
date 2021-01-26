fc84e4614ffcf8917c3b637092379bb0d4369530
thiha <mr.alvin199818@gmail.com>
2021-01-27 01:26:59 +0630

    New Export

---
+++ resources/views/magic/landing.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAGIC MOBILE</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+</head>
+<body>
+    <div id="main" class="animate__animated animate__fadeInLeft" style="height: 500px;">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#" style="font-size: 50px;">LOGO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">ABOUT<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">CONTACT US<span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="col-md-12 mt-4" style="text-align:center;">
+            <h1 class="h animate__animated animate__fadeInUp" >MAGIC MOBILE</h1>
+            <p class="p animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate impedit eaque temporibus corporis, corrupti dicta ex totam et numquam quibusdam? Eum amet atque sint, placeat quae pariatur provident harum in.</p>
+            <div class="b d-flex justify-content-center animate__animated animate__fadeInUp mt-5">
+                <button id="button-m" class="btn btn-outline-dark">Accessories</button>
+                <button id="button-m" class="btn btn-outline-dark ml-1">Service Center</button>
+                <button id="button-m" class="btn btn-outline-dark ml-1">Mobile Knowledge</button>
+                <button id="button-m" class="btn btn-outline-dark ml-1">Second Hand Mobile</button>
+            </div>
+        </div>
+    </div>
+    <div class="accessory animate__animated animate__fadeInRight" style="height: 200px; background-image: url('../images/BlackBerry KeyOne - Gear Hungry.jpeg'); background-size: cover; background-repeat: no-repeat;">
+        <div class="col-md-12 acc">
+            <h1 class="pt-3 access-text" style="letter-spacing: 20px; opacity: 0.7; font-size: 50px;"><strong style="font-size: 60px; font-family: 'Cardo', serif;">A</strong>CCESSORIES</h1>
+            <button class="bottomright btn btn-outline-dark">VIEW ITEMS</button>
+        </div>
+    </div>
+    <div class="service-center animate__animated animate__fadeInLeft" style="height: 200px; background-image: url('../images/service.jpeg'); background-size: cover; background-repeat: no-repeat;">
+        <div class="col-md-12 acc">
+            <h1 class="pt-3 access-text" style="letter-spacing: 20px; opacity: 0.7; font-size: 50px;"><strong style="font-size: 60px; font-family: 'Cardo', serif;">S</strong>ERVICE CENTER</h1>
+            <button class="bottomright btn btn-outline-dark">ENTER THE CENTER</button>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
